ff01376232f66fe1d7ec57c9cfed18365cbfe411
glmrvn <glmrvn@gmail.com>
2020-05-06 07:56:45 +0300

    Find only frames and instances

--- code.js
+++ code.js
@@ -2,9 +2,8 @@
 
 const frames = figma.currentPage.findAll(node => node.type === "FRAME" && node.parent.type != "FRAME")
 const instances = figma.currentPage.findAll(node => node.type === "INSTANCE" && node.parent.type != "INSTANCE" && node.parent.type != "FRAME")
-const groups = figma.currentPage.findAll(node => node.type === "GROUP" && node.parent.type === "PAGE")
 
-const allNodes = [...frames, ...instances, ...groups]
+const allNodes = [...frames, ...instances]
 
 // ----------------------------------FIND DUPLICATES------------------------------------
 
@@ -17,6 +16,10 @@ var duplicates = Object.values(allNodes.reduce((c, v) => {
   }, {})).reduce((c, v) => v.length > 1 ? c.concat(v) : c, []);
 
 // ----------------------------------SELECT OBJECTS-------------------------------------
-
-figma.currentPage.selection = duplicates
-figma.closePlugin()
+if (duplicates.length > 0) {
+  figma.currentPage.selection = duplicates
+  figma.viewport.scrollAndZoomIntoView(duplicates)
+  figma.closePlugin("🚨Duplicates")
+} else {
+  figma.closePlugin("👌You don't have frames and instances duplicates")
+}
